418be6e557cfc1eedf6cff4b1c8f24e7b3af9a76
devendran <devendranvemula@gmail.com>
2022-01-25 12:50:24 +0530

    projects added

---
+++ src/pages/projects/project-list.js
@@ -0,0 +1,59 @@
+const projectsDetails = [
+  {
+    projectName: "Raaonline",
+    projectImgUrl: "/assets/raa.png",
+    githubUrl: "",
+    webUrl: "https://www.raaonline.co.in/",
+  },
+  {
+    projectName: "BangaloreWaits",
+    projectImgUrl: "/assets/blr.png",
+    githubUrl: "",
+    webUrl: "",
+  },
+  {
+    projectName: "Lamp",
+    projectImgUrl: "/assets/lamp.png",
+    githubUrl: "",
+    webUrl: "https://lamp-customer-portal.web.app/",
+    playstoreLink: "",
+  },
+  {
+    projectName: "PEC Student",
+    projectImgUrl: "/assets/pecstudent.png",
+    githubUrl: "https://github.com/vemuladevendran/student-ui",
+    webUrl: "https://pecstudents.engineer/students",
+  },
+  {
+    projectName: "Mini Stucor",
+    projectImgUrl: "/assets/mini-stucor.png",
+    githubUrl: "https://github.com/vemuladevendran/studentui",
+    webUrl: "https://studentportel-1.web.app/login",
+    playstoreLink:
+      "https://play.google.com/store/apps/details?id=io.devendran.student",
+  },
+  {
+    projectName: "Flames",
+    projectImgUrl: "/assets/flames.jpg",
+    githubUrl: "https://github.com/vemuladevendran/flames",
+    webUrl: "",
+    playstoreLink:
+      "https://play.google.com/store/apps/details?id=com.devendran.flamesapp",
+  },
+  {
+    projectName: "PEC Admin",
+    projectImgUrl: "/assets/pecadmin.png",
+    githubUrl: "https://github.com/vemuladevendran/pec-admin",
+    webUrl: "https://pecadminui.web.app/teachers",
+    playstoreLink: "",
+  },
+  {
+    projectName: "Sri Krishna Dairy",
+    projectImgUrl: "/assets/srk.png",
+    githubUrl: "",
+    webUrl: "https://srikrishnadairy.in/sales/new",
+    playstoreLink: "",
+  },
+];
+
+export default projectsDetails;
--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,21 +1,7 @@
 import "./project.css";
+import projectsDetails from "./project-list";
 
 function Projects() {
-  const projectsDetails = [
-    {
-      projectName: "Raaonline",
-      projectImgUrl: "/assets/raa.png",
-      githubUrl: "",
-      webUrl: "www.raaonline.co.in/",
-    },
-    {
-      projectName: "BangaloreWaits",
-      projectImgUrl: "/assets/blr.png",
-      githubUrl: "",
-      webUrl: "",
-    },
-  ];
-
   return (
     <div className="container-fluid py-5" style={{ background: "#212529" }}>
       <div className="container">
@@ -23,29 +9,52 @@ function Projects() {
           <h1 className="project-text mb-3">Projects</h1>
 
           {projectsDetails.map((x) => {
-            {
-              /* project cards */
-            }
             return (
-              <div className="col-12 col-md-4 card-shadow">
-                <p className="project-text">{x.projectName}</p>
-                <div>
-                  <img src={x.projectImgUrl} alt="raa" className="w-100 project-img" />
-                  <div className="d-flex justify-content-between mt-1">
-                    <button className="details-btn border-0">
-                      <p className="ani-text">
-                        <span>M</span>
-                        ore
-                        <span>Details</span>
-                      </p>
-                    </button>
-                    <div className="project-text">
-                      <a href={x.webUrl} target="_blank" rel="noreferrer">
-                        <i className="bi bi-box-arrow-up-right text-white fw-bold mx-2"></i>
-                      </a>
-                      <a href={x.githubUrl} target="_blank" rel="noreferrer">
-                        <i className="bi bi-github text-white fw-bold mx-1"></i>
-                      </a>
+              <div className="col-12 col-md-4 p-3">
+                <div className="card-shadow">
+                  <p className="project-text">{x.projectName}</p>
+                  <div>
+                    <img
+                      src={x.projectImgUrl}
+                      alt="raa"
+                      className="w-100 project-img"
+                    />
+                    <div className="d-flex justify-content-between mt-1">
+                      <button className="details-btn border-0">
+                        <p className="ani-text">
+                          <span>M</span>
+                          ore
+                          <span>Details</span>
+                        </p>
+                      </button>
+                      <div className="project-text">
+                        {x.webUrl ? (
+                          <a href={x.webUrl} target="_blank" rel="noreferrer">
+                            <i className="bi bi-box-arrow-up-right text-white fw-bold mx-2"></i>
+                          </a>
+                        ) : null}
+                        {x.githubUrl ? (
+                          <a
+                            href={x.githubUrl}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <i className="bi bi-github text-white fw-bold mx-1"></i>
+                          </a>
+                        ) : null}
+                        {x.playstoreLink ? (
+                          <a
+                            href={x.playstoreLink}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <i
+                              className="bi bi-shop text-white fw-bold mx-2"
+                              title="play store"
+                            ></i>
+                          </a>
+                        ) : null}
+                      </div>
                     </div>
                   </div>
                 </div>
